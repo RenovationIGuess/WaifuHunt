67eddad9cb62b8960f5b1d389f9aeb5f3557baab
ntr <nguyen.tung.lam-b@sun-asterisk.com>
2022-08-04 11:22:50 +0700

    second_commit

--- server/config/db/index.js
+++ server/config/db/index.js
@@ -3,21 +3,21 @@ require("dotenv").config();
 
 async function connect() {
   try {
-    await mongoose.connect(process.env.DATABASE_URL, {
+    /* await mongoose.connect(process.env.DATABASE_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    });
+    }); */
     /* await mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@waifuhunt.b7xga8c.mongodb.net/WaifuHunt`, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         }); */
-    /* await mongoose.connect(
+    await mongoose.connect(
       `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@waifuhunt.b7xga8c.mongodb.net/WaifuHunt?retryWrites=true&w=majority`,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
       }
-    ); */
+    );
     /* const db = mongoose.connection;
         db.once('open', () => console.log('Connected to the database')); */
     console.log("Connected to the database");
